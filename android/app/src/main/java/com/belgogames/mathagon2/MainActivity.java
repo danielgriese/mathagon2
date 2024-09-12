6347783a94ee91c6455fcd1321a5af47a4596538
Daniel Griese <daniel.griese@zlyde.com>
2024-09-12 18:57:47 +0200

    added capacitor android project

---
+++ android/app/src/main/java/com/belgogames/mathagon2/MainActivity.java
@@ -0,0 +1,5 @@
+package com.belgogames.mathagon2;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.belgogames.mathagon2",
+  appName: "Mathagon 2",
+  webDir: "out",
+};
+
+export default config;
